f390297f8bf9eeed12eaeab1f6d581a521760428
Bootcamp <bootcamp@H5663.ad.hugeinc.com>
2017-11-15 11:57:15 -0500

    Upload testing

---
+++ script.test.js
@@ -0,0 +1,51 @@
+import{
+    generateRandomColor,
+    filterNegativeNumbers,
+    functionalfilterNegativeNumbers,
+    mapNumbersIntoStrings,
+    functionalMapNumbersIntoStrings,
+    printType,
+    isPalindrome,
+    functionalisPalindrome
+}from './index.js';
+
+describe('generateRandomColor',()=>{
+    it('should work',()=>{
+        expect(1).toBe(1);
+    });
+});
+describe('filterNegativeNumbers',()=>{
+    it('should work',()=>{
+        expect(1).toBe(1);
+    });
+});
+describe('functionalfilterNegativeNumbers',()=>{
+    it('should work',()=>{
+        expect(1).toBe(1);
+    });
+});
+describe('mapNumbersIntoStrings',()=>{
+    it('should work',()=>{
+        expect(1).toBe(1);
+    });
+});
+describe('functionalMapNumbersIntoStrings',()=>{
+    it('should work',()=>{
+        expect(1).toBe(1);
+    });
+});
+describe('printType',()=>{
+    it('should work',()=>{
+        expect(1).toBe(1);
+    });
+});
+describe('isPalindrome',()=>{
+    it('should work',()=>{
+        expect(1).toBe(1);
+    });
+});
+describe('functionalisPalindrome',()=>{
+    it('should work',()=>{
+        expect(1).toBe(1);
+    });
+});
