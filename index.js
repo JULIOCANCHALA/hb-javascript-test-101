0739a36d00ce419a6bae2383c542514f00cbec7e
JULIOCANCHALA <juliofernando1c@hotmail.com>
2017-11-17 18:08:38 -0500

    Uploading from Windows

--- index.js
+++ index.js
@@ -1,91 +1,89 @@
 /*1. Function that is capable of generate a random RGB color object*/
-export function generateRandomColor(){
+export function generateRandomColor() {
     return {
-        "r":Math.floor(Math.random()*255),
-        "g":Math.floor(Math.random()*255),
-        "b":Math.floor(Math.random()*255)
+        "r": Math.floor(Math.random() * 255),
+        "g": Math.floor(Math.random() * 255),
+        "b": Math.floor(Math.random() * 255)
     };
 }
 
 /*2. Function that is capable of filtering out numbers less than 0 from an array of numbers*/
 /*Loops*/
-export function filterNegativeNumbers(arr){
-    var i=0;
-    const arr2=[];
-    for(i=0;i<arr.length;i++){
-        if (arr[i]>=0){
+export function filterNegativeNumbers(arr) {
+    var i = 0;
+    const arr2 = [];
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] >= 0) {
             arr2.push(arr[i]);
         }
     }
     return arr2;
 }
 /*Array methods*/
-export function functionalfilterNegativeNumbers(arr){
-    return arr.filter(function(item){return item >= 0;})    
+export function functionalfilterNegativeNumbers(arr) {
+    return arr.filter(function(item) { return item >= 0; })
 }
 
 /*3. Function that is capable of maping an array of numbers into strings*/
 /*Loops*/
-export function mapNumbersIntoStrings(arr){
-    var i=0;
-    const arr2=[];
-    for(i=0;i<arr.length;i++){
-            arr2.push(arr[i].toString());
+export function mapNumbersIntoStrings(arr) {
+    var i = 0;
+    const arr2 = [];
+    for (i = 0; i < arr.length; i++) {
+        arr2.push(arr[i].toString());
     }
     return arr2;
 }
 
 /*Array methods*/
-export function functionalMapNumbersIntoStrings(arr){
-    return arr.map(function(item){return item.toString()})
+export function functionalMapNumbersIntoStrings(arr) {
+    return arr.map(function(item) { return item.toString() })
 }
 
 /*4. Create a function that is capable of printing into the console the type of the passed variable*/
-export function printType(item){
+export function printType(item) {
     console.log(typeof item);
 }
 
 /*5. Function that is capable of identify if the passed string is a palindrome or not */
 /*Loops*/
-export function isPalindrome(str){
-    var i=0;
-    var str2=str.replace(/[^aA-z0-9]/ig,'');
-    str2=str2.toLowerCase();
-    var cnt=0;
-    var result=false;
-    for(i=0;i<(Math.floor(str2.length/2));i++){
-        if(str2[i]===str2[str2.length-i-1]){
+export function isPalindrome(str) {
+    var i = 0;
+    var str2 = str.toLowerCase().replace(/[^aA-z0-9]/ig, '');
+    var cnt = 0;
+    var result = false;
+    for (i = 0; i < (Math.floor(str2.length / 2)); i++) {
+        if (str2[i] === str2[str2.length - i - 1]) {
             cnt++;
         }
     }
-    if(cnt==Math.floor(str2.length/2)){
-        result=true;
+    if (cnt == Math.floor(str2.length / 2)) {
+        result = true;
     }
     return result;
 }
 
 /*Array methods*/
-export function functionalisPalindrome(str){
-    var str2=str.replace(/[^aA-z0-9]/ig,'');
-    str2=str2.toLowerCase();
-    return str2===str2.split('').reverse().join('');
+export function functionalisPalindrome(str) {
+    var str2 = str.toLowerCase().replace(/[^aA-z0-9]/ig, '');
+    return str2 === str2.split('').reverse().join('');
 }
 
 
 /*6. Class in Javascript that represents a person capable of having “name” and “age” as instance variables*/
-export class Person{
-    constructor(name,age){
-    this.name=name;
-    this.age=age;
+export class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
     }
-    printName(){
+    printName() {
         console.log(this.name);
     }
-    get Age(){
+    get Age() {
         return this.age;
     }
 }
 
-export function printOutPersonAge(person){
+export function printOutPersonAge(person) {
     console.log(person.Age);
 }
--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-import{
+import {
     generateRandomColor,
     filterNegativeNumbers,
     functionalfilterNegativeNumbers,
@@ -9,87 +9,87 @@ import{
     functionalisPalindrome,
     Person,
     printOutPersonAge
-}from './index.js';
+} from './index.js';
 
-describe('generateRandomColor',()=>{
-    it('should work',()=>{
-        const aux={
-            r:expect.any(Number),
-            g:expect.any(Number),
-            b:expect.any(Number)
+describe('generateRandomColor', () => {
+    it('should work', () => {
+        const aux = {
+            r: expect.any(Number),
+            g: expect.any(Number),
+            b: expect.any(Number)
         }
         expect(generateRandomColor()).toMatchObject(aux);
     });
 });
 
-describe('filterNegativeNumbers',()=>{
-    it('show array without negative numbers',()=>{
-        const aux=[-1,-2,-5,76,4,0,2,3,0,-5];
-        const res=[76,4,0,2,3,0];
+describe('filterNegativeNumbers', () => {
+    it('show array without negative numbers', () => {
+        const aux = [-1, -2, -5, 76, 4, 0, 2, 3, 0, -5];
+        const res = [76, 4, 0, 2, 3, 0];
         expect(filterNegativeNumbers(aux)).toEqual(res);
         expect(functionalfilterNegativeNumbers(aux)).toEqual(res);
         expect(filterNegativeNumbers([0])).toEqual([0]);
         expect(functionalfilterNegativeNumbers([0])).toEqual([0]);
     })
 
-    it('show empty array if all values are negatives',()=>{
-        expect(filterNegativeNumbers([-2,-50,-3])).toEqual([]);
-        expect(functionalfilterNegativeNumbers([-2,-50,-3])).toEqual([]);
+    it('show empty array if all values are negatives', () => {
+        expect(filterNegativeNumbers([-2, -50, -3])).toEqual([]);
+        expect(functionalfilterNegativeNumbers([-2, -50, -3])).toEqual([]);
     })
 
-    it('should not modify the original array',()=>{
-        const arr=[1,2,-3,4,-20,0];
-        const copy=arr;
+    it('should not modify the original array', () => {
+        const arr = [1, 2, -3, 4, -20, 0];
+        const copy = arr;
         filterNegativeNumbers(arr);
         expect(arr).toEqual(copy);
     })
 
-    it('should not modify the original array - functional',()=>{
-        const arr=[0,0,1,13,-2,4,0];
-        const copy=arr;
+    it('should not modify the original array - functional', () => {
+        const arr = [0, 0, 1, 13, -2, 4, 0];
+        const copy = arr;
         functionalfilterNegativeNumbers(arr);
         expect(arr).toEqual(copy);
     })
 
 })
 
-describe('mapNumbersIntoStrings',()=>{
-    it('show array of strings',()=>{
-        const arr=[1,2,-3,4,-20,0];
-        const res=['1','2','-3','4','-20','0'];
+describe('mapNumbersIntoStrings', () => {
+    it('show array of strings', () => {
+        const arr = [1, 2, -3, 4, -20, 0];
+        const res = ['1', '2', '-3', '4', '-20', '0'];
         expect(mapNumbersIntoStrings(arr)).toEqual(res);
         expect(functionalMapNumbersIntoStrings(arr)).toEqual(res);
     })
 
-    it('show empty array if original array is empty',()=>{
+    it('show empty array if original array is empty', () => {
         expect(filterNegativeNumbers([])).toEqual([]);
         expect(functionalfilterNegativeNumbers([])).toEqual([]);
     })
 
-    it('should not modify the original array',()=>{
-        const arr=[1,2,-3,4,-20,0];
-        const copy=arr;
+    it('should not modify the original array', () => {
+        const arr = [1, 2, -3, 4, -20, 0];
+        const copy = arr;
         mapNumbersIntoStrings(arr);
         expect(arr).toEqual(copy);
     })
 
-    it('should not modify the original array - functional',()=>{
-        const arr=[0,0,1,13,-2,4,0];
-        const copy=arr;
+    it('should not modify the original array - functional', () => {
+        const arr = [0, 0, 1, 13, -2, 4, 0];
+        const copy = arr;
         functionalMapNumbersIntoStrings(arr);
         expect(arr).toEqual(copy);
     })
 
 })
 
-describe('printType',()=>{
-    beforeEach(()=>{
-        console={
-            log:jest.fn()//recibe los parametros que recibe la funcion console.log
-        }//hago esto para con la esa nueva funcion que reescribio al console.log saber si la llamaron y con que parametro la llamaron
+describe('printType', () => {
+    beforeEach(() => {
+        console = {
+                log: jest.fn() //recibe los parametros que recibe la funcion console.log
+            } //hago esto para con la esa nueva funcion que reescribio al console.log saber si la llamaron y con que parametro la llamaron
     })
 
-    it('show type of input',()=>{
+    it('show type of input', () => {
         printType('a');
         expect(console.log).toBeCalledWith('string');
     })
@@ -98,9 +98,11 @@ describe('printType',()=>{
 describe('isPalindrome', () => {
     it('show true with a palindrome word', () => {
         expect(isPalindrome('maadaam')).toBeTruthy();
+        expect(functionalisPalindrome('maadaam')).toBeTruthy();
     });
     it('show true with a palindrome phrase', () => {
         expect(isPalindrome("A man, a plan, a canal. Panama")).toBeTruthy();
+        expect(functionalisPalindrome("A man, a plan, a canal. Panama")).toBeTruthy();
     });
 });
 
@@ -133,8 +135,8 @@ describe('Class Person', () => {
 describe('printOutPersonAge', () => {
     beforeEach(() => {
         global.console = {
-            log: jest.fn()
-        } // Creating a spy function for the console.log
+                log: jest.fn()
+            } // Creating a spy function for the console.log
     });
 
     it('should print in console the age of the given instance', () => {
@@ -142,4 +144,3 @@ describe('printOutPersonAge', () => {
         expect(global.console.log).toHaveBeenCalledWith(100);
     });
 });
-
