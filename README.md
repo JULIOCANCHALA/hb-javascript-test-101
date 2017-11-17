2294c4064f502be399184d2fa9417402c9eb228e
Bootcamp <bootcamp@H5663.ad.hugeinc.com>
2017-11-17 10:45:33 -0500

    add travis, codecov and shields

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hb-javascript-test-101
+[![Travis](https://img.shields.io/travis/JULIOCANCHALA/hb-javascript-test-101.svg)](https://travis-ci.org/andrscrrn/hb-javascript-test-101)
+[![Codecov](https://codecov.io/gh/JULIOCANCHALA/hb-javascript-test-101/branch/master/graph/badge.svg)](https://codecov.io/gh/andrscrrn/hb-javascript-test-101)
--- script.test.js
+++ script.test.js
@@ -93,4 +93,51 @@ describe('printType',()=>{
     })
 })
 
+describe('isPalindrome', () => {
+    it('show true with a palindrome word', () => {
+        expect(isPalindrome('maadaam')).toBeTruthy();
+    });
+    it('show true with a palindrome phrase', () => {
+        expect(isPalindrome("A man, a plan, a canal. Panama")).toBeTruthy();
+    });
+});
+
+//****************FUNCIONES FALTANTES
+
+describe('Person', () => {
+    let personInstance;
+
+    beforeAll(() => {
+        personInstance = new Person('John', 100);
+
+        global.console = {
+            log: jest.fn()
+        }; // Creating a spy function for the console.log
+    });
+
+    it('should return an instance with the correct values when calling it as a constructor', () => {
+        expect(personInstance).toMatchObject({
+            name: 'John',
+            age: 100
+        });
+    });
+
+    it('should print the Person instance name in the console', () => {
+        personInstance.printName();
+        expect(global.console.log).toHaveBeenCalledWith('John');
+    });
+});
+
+describe('printOutPersonAge', () => {
+    beforeEach(() => {
+        global.console = {
+            log: jest.fn()
+        } // Creating a spy function for the console.log
+    });
+
+    it('should print in console the age of the given instance', () => {
+        printOutPersonAge(new Person('John', 100));
+        expect(global.console.log).toHaveBeenCalledWith(100);
+    });
+});
 
